229cc2c4087136ed71ce2ccf7a095f5b71514117
AlessandroCorsetti <Corsect91@hotmail.it>
2022-05-25 14:49:36 +0200

    Creazione Team

--- force-app/main/default/lwc/creazioneRosaLwc/creazioneRosaLwc.js
+++ force-app/main/default/lwc/creazioneRosaLwc/creazioneRosaLwc.js
@@ -4,9 +4,30 @@ export default class CreazioneRosaLwc extends LightningElement {
 
     //con l'annotation @api indico che è una variabile pubblica dato che è stata creata dal padre e in questo caso la stiamo passando al figlio
     @api team;
+    valueCombo = 'Seleziona Ruolo';
+    errorMessage;
+    errorCall = false;
+
+    get options() {
+        return [
+            { label: 'Portiere', value: 'Por' },
+            { label: 'Difensore', value: 'Dif' },
+            { label: 'Centrocampista', value: 'Ccr' },
+            { label: 'Attaccante', value: 'Att' }
+        ];
+    }
+    handleChange(event) {
+
+        if(event.name == 'Ruolo__c'){
+
+            this.valueCombo = event.target.value;
+
+
+        }
 
 
 
+    }
 
 
 
